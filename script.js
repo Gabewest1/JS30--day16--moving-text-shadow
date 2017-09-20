baf93d43774ebf1c613a8d5e378b0e3c2774175e
Gabriel West <gabe_west1@yahoo.com>
2017-09-20 15:05:07 -0500

    Gave the words different colors. Made main word have a higher z-index. Decreased the amount i multiply the result of the mouses location minus the center of the screen.

--- script.js
+++ script.js
@@ -10,18 +10,19 @@
             top: window.innerHeight / 2
         }
         console.log(words)
-        let left = ((centerOfScreen.left - clientX)) * .8 //The multiplication stops the words from going off the screen
-        let top = ((centerOfScreen.top - clientY)) * .8 //The multiplication stops the words from going off the screen
+        let left = ((centerOfScreen.left - clientX)) * .5 //The multiplication stops the words from going off the screen
+        let top = ((centerOfScreen.top - clientY)) * .5 //The multiplication stops the words from going off the screen
         console.log("TOP AND LEFT:", top, left)
         //Start by modyfiying the bottom word
         words.forEach((word, index) => {
+            console.log("ayy:", word, index)
             switch (index) {
                 case 0: {
                     word.style.setProperty("transform", `translate(${-left}px, ${top}px)`)
                     break
                 }
                 case 1: {
-                    word.style.setProperty("transform", `translate${left}px, ${top}px)`)
+                    word.style.setProperty("transform", `translate(${left}px, ${top}px)`)
                     break
                 }
                 case 2: {
